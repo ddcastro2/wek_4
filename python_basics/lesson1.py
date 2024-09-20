7c9ea58e75ab035a64f41f32a46f833ae637a735
Castro <ddcastro1@cps.k12.il.us>
2024-09-20 14:37:24 -0500

    first commit

---
+++ python_basics/lesson1.py
@@ -0,0 +1,24 @@
+#mkdir means make a directory
+#cd means change directory
+#ls means list directory
+#pwd means print working directory
+#touch means create a file
+#rm means remove a file
+# # pound sign means comment
+# print ("Hello, World!")
+# print ("Damian")
+# print("age \t 25")
+# # \t means tab
+# print("Ashburn")
+# ask for input
+name = input("What is your name? ")
+print("Hello, " + name)
+age = input("What is your age? ")
+print("You are " + age)
+address = input("What is your address? ")
+print("You live in " + address)
+school = input("What school are you from? ")
+print("You go to " + school)
+place_of_birth = input("What is your place of birth? ")
+print("You were born in " + place_of_birth)
+print("So, " + name + " is " + age + " years old" + ", lives in " + address + ", goes to " + school + " and was born in " + place_of_birth)
---
+++ python_basics/week2.py
@@ -0,0 +1,9 @@
+#ask someone what day it is
+day = input("What day is it? ")
+print("Today is " + day)
+#ask them for their favorite holiday
+fav_Holiday = input("What is your favorite holiday? ")
+print("You favorite holiday is " + fav_Holiday)
+#print out the day and their favorite holiday in a sentence
+print("Today is " + day + " and your favorite holiday is " + fav_Holiday)
+#making a change to the file

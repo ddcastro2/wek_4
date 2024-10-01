93500e25bf59e7e3b58642d58c9bf68689b41dd9
Castro <ddcastro1@cps.k12.il.us>
2024-10-01 14:43:00 -0500

    capitals and tuples

--- week_4/lists.py
+++ week_4/lists.py
@@ -1,38 +1,48 @@
-cars = ["Ford", "Volvo", "BMW", "Mitsubishi", "Toyota", "Lamborghini", "Rolls Royce"]
-cars.append("Honda")
-print(f"The cars in the list are: {cars}")
-cars[-1] = "Austin Martin"
-cars[2] = "Chevy"
-print(f"The list of cars are now: {cars}")
-cars.insert(2, "Jeep")
-print(f"The list of cares are now: {cars}")
-cars.remove("Jeep")
-print(f"The list of cares are now: {cars}")
-check = ("Ford" in cars)
-print(f"The statement that Ford is in the list is {check}")
+# cars = ["Ford", "Volvo", "BMW", "Mitsubishi", "Toyota", "Lamborghini", "Rolls Royce"]
+# cars.append("Honda")
+# print(f"The cars in the list are: {cars}")
+# cars[-1] = "Austin Martin"
+# cars[2] = "Chevy"
+# print(f"The list of cars are now: {cars}")
+# cars.insert(2, "Jeep")
+# print(f"The list of cares are now: {cars}")
+# cars.remove("Jeep")
+# print(f"The list of cares are now: {cars}")
+# check = ("Ford" in cars)
+# print(f"The statement that Ford is in the list is {check}")
 
-# for car in cars:
-#     requestCar = input("Enter a car: ")
-#     cars.append(requestCar)
-#     print(f'The cars in the list are: {cars}')
-#     if len(cars) > 10:
-#         print("You have reached the maximum amount of cars.")
+# # for car in cars:
+# #     requestCar = input("Enter a car: ")
+# #     cars.append(requestCar)
+# #     print(f'The cars in the list are: {cars}')
+# #     if len(cars) > 10:
+# #         print("You have reached the maximum amount of cars.")
+# #       (break)
+
+
+
+# print("               ")
+# print("this is this seperate the two challenges")
+# print("               ")
+
+
+# #CHALLENGE
+# #create a list of friends in the list
+# friends = []
+# for friend in friends:
+#     requestFriend = input("Enter a friend: ")
+#     friends.append(requestFriend)
+#     print(f'Here is my list of friends: {friends}')
+#     if len(cars) >= 4:
+#         print("You're not allowed to insert anymore friends.")
 #         break
 
+# # print(f"Here is my list of friends: {friends}")
+# # friends[3] = "Adrian"
+# # print(f"My new list of friends is: {friends}")
+# # friends[2] = "Aidan"
+# # print(f"My new list of friends is: {friends}")
+# # friends.insert(2, "Jonathan")
+# # print(f"My new list of friends is: {friends}")
 
-
-print("               ")
-print("this is this seperate the two challenges")
-print("               ")
-
-
-#CHALLENGE
-#create a list of friends in the list
-friends = ["Miguel", "Fernando", "Jonathan","Aidan"]
-print(f"Here is my list of friends: {friends}")
-friends[3] = "Adrian"
-print(f"My new list of friends is: {friends}")
-friends[2] = "Aidan"
-print(f"My new list of friends is: {friends}")
-friends.insert(2, "Jonathan")
-print(f"My new list of friends is: {friends}")
+###########
